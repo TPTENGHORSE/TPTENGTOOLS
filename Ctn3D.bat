0cb9d01ceea9852dff0743487678e58ab82b07f0
OLMEDO Jorge <jorge.olmedo@horse.tech>
2025-06-20 09:03:43 +0200

    Guardando cambios antes de sincronizar con remoto

--- Ctn3D.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activar entorno virtual
-call .venv\Scripts\activate
-
-REM Ejecutar la app
-streamlit run Container3D.py
-
-REM Mantener la ventana abierta al final (opcional)
-pause
